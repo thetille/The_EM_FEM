ad81fdb622ade5b5196ca7f9a2d90f88509f622a
thethielle <jthiel16@student.aau.dk>
2021-03-09 14:13:19 +0100

    final code walkthough

--- matlab/3D/current/Fem_Assemble.m
+++ matlab/3D/current/Fem_Assemble.m
@@ -61,50 +61,53 @@ for elIdx = 1:elNumGlo % goes throug the amount of thetras
     noTmp = zeros(size(ed2noLoc)); % temp var of size of the amount of initial base lines (edges) 
     noTmp(:) = el2no(ed2noLoc(:),elIdx); % temp var with initial base edge nodes (one edge has 2 nodes ie 6x2)
     esVtr = sign(noTmp(2,:)-noTmp(1,:)); % each edge gest an assigned direction based on node id (this is random)is only used as initial value
-    eiVtr = ElementDatabase_Get('edges', noTmp); % each edge assosiated with the specific thetra
+    eiVtr = ElementDatabase_Get('edges', noTmp); % each edge id assosiated with the specific thetra (6 edges)
     
     %for faces
     noTmp = zeros(size(fa2noLoc)); % temp var of size of the amount of initial faces
     noTmp(:) = el2no(fa2noLoc(:),elIdx); % temp var with initial nodes of each face
-    fsVtr = 2*(... % each edge gets a normal vector, initial direction is based on index only
+    fsVtr = 2*(... % each face gets a normal vector, initial direction is based on index only
         ((noTmp(1,:) < noTmp(2,:)) & (noTmp(2,:) < noTmp(3,:))) | ...
         ((noTmp(2,:) < noTmp(3,:)) & (noTmp(3,:) < noTmp(1,:))) | ...
         ((noTmp(3,:) < noTmp(1,:)) & (noTmp(1,:) < noTmp(2,:))) ...
         ) - 1;
     fiVtr = ElementDatabase_Get('faces', noTmp); % gets ids for faces assosiated with the specific thetra
-        
-    irTmp_EE = eiVtr'*ones(size(eiVtr));
-    icTmp_EE = ones(size(eiVtr'))*eiVtr;
-    isTmp_EE = esVtr'*esVtr;
     
-    irTmp_FE = fiVtr'*ones(size(eiVtr));
-    icTmp_FE = ones(size(fiVtr'))*eiVtr;
-    isTmp_FE = fsVtr'*esVtr;
+    % edges only
+    irTmp_EE = eiVtr'*ones(size(eiVtr)); % row index for eMtx only based on edge id
+    icTmp_EE = ones(size(eiVtr'))*eiVtr; % column index for eMtx only based on edge id
+    isTmp_EE = esVtr'*esVtr; % The direction adjusted for the eElMtx_EE and sElMtx_EE matrix
     
-    irTmp_FF = fiVtr'*ones(size(fiVtr));
-    icTmp_FF = ones(size(fiVtr'))*fiVtr;
-    isTmp_FF = fsVtr'*fsVtr;
+    % faces and edges
+    irTmp_FE = fiVtr'*ones(size(eiVtr)); % row index for cMtx only based on face and edge id
+    icTmp_FE = ones(size(fiVtr'))*eiVtr;  % column index for cMtx only based on face and edge id
+    isTmp_FE = fsVtr'*esVtr; % The direction adjusted for the cElMtx_FE matrix
     
-    irRes_EE(idxRes_EE + (1:incRes_EE) - 1) = irTmp_EE(:);
-    icRes_EE(idxRes_EE + (1:incRes_EE) - 1) = icTmp_EE(:);
-    meRes_EE(idxRes_EE + (1:incRes_EE) - 1) = isTmp_EE(:).*eElMtx_EE(:);
-    msRes_EE(idxRes_EE + (1:incRes_EE) - 1) = isTmp_EE(:).*sElMtx_EE(:);
+    % faces only
+    irTmp_FF = fiVtr'*ones(size(fiVtr)); % row index for uMtx only based on face id
+    icTmp_FF = ones(size(fiVtr'))*fiVtr; % column index for uMtx only based on face id
+    isTmp_FF = fsVtr'*fsVtr; % The direction adjusted for the uElMtx_FF matrix
     
-    irRes_FE(idxRes_FE + (1:incRes_FE) - 1) = irTmp_FE(:);
-    icRes_FE(idxRes_FE + (1:incRes_FE) - 1) = icTmp_FE(:);
-    mcRes_FE(idxRes_FE + (1:incRes_FE) - 1) = isTmp_FE(:).*cElMtx_FE(:);
+    irRes_EE(idxRes_EE + (1:incRes_EE) - 1) = irTmp_EE(:); % includes all row indexes for all thetras for eMtx and sMtx
+    icRes_EE(idxRes_EE + (1:incRes_EE) - 1) = icTmp_EE(:); % includes all column indexis for all thetras for eMtx and sMtx
+    meRes_EE(idxRes_EE + (1:incRes_EE) - 1) = isTmp_EE(:).*eElMtx_EE(:); % resulting data for eMtx
+    msRes_EE(idxRes_EE + (1:incRes_EE) - 1) = isTmp_EE(:).*sElMtx_EE(:); % resultingdata for sMtx
     
-    irRes_FF(idxRes_FF + (1:incRes_FF) - 1) = irTmp_FF(:);
-    icRes_FF(idxRes_FF + (1:incRes_FF) - 1) = icTmp_FF(:);
-    muRes_FF(idxRes_FF + (1:incRes_FF) - 1) = isTmp_FF(:).*uElMtx_FF(:);
+    irRes_FE(idxRes_FE + (1:incRes_FE) - 1) = irTmp_FE(:); % includes all row indexes for all thetras for cMtx
+    icRes_FE(idxRes_FE + (1:incRes_FE) - 1) = icTmp_FE(:); % includes all column indexis for all thetras for cMtx
+    mcRes_FE(idxRes_FE + (1:incRes_FE) - 1) = isTmp_FE(:).*cElMtx_FE(:); % resultingdata for cMtx
     
-    idxRes_EE = idxRes_EE + incRes_EE;
-    idxRes_FE = idxRes_FE + incRes_FE;
-    idxRes_FF = idxRes_FF + incRes_FF;
+    irRes_FF(idxRes_FF + (1:incRes_FF) - 1) = irTmp_FF(:); % includes all row indexes for all thetras for uMtx
+    icRes_FF(idxRes_FF + (1:incRes_FF) - 1) = icTmp_FF(:); % includes all column indexis for all thetras for uMtx
+    muRes_FF(idxRes_FF + (1:incRes_FF) - 1) = isTmp_FF(:).*uElMtx_FF(:); % resultingdata for uMtx
+    
+    idxRes_EE = idxRes_EE + incRes_EE; % incerment pointer for edge edge
+    idxRes_FE = idxRes_FE + incRes_FE; % incerment pointer for face edge
+    idxRes_FF = idxRes_FF + incRes_FF; % incerment pointer for face face
     
 end
 
-eMtx = sparse(irRes_EE, icRes_EE, meRes_EE, edNumGlo, edNumGlo);
-sMtx = sparse(irRes_EE, icRes_EE, msRes_EE, edNumGlo, edNumGlo);
-cMtx = sparse(irRes_FE, icRes_FE, mcRes_FE, faNumGlo, edNumGlo);
-uMtx = sparse(irRes_FF, icRes_FF, muRes_FF, faNumGlo, faNumGlo);
+eMtx = sparse(irRes_EE, icRes_EE, meRes_EE, edNumGlo, edNumGlo); % M^{epsilon}
+sMtx = sparse(irRes_EE, icRes_EE, msRes_EE, edNumGlo, edNumGlo); % M^{sigma}
+cMtx = sparse(irRes_FE, icRes_FE, mcRes_FE, faNumGlo, edNumGlo); % C
+uMtx = sparse(irRes_FF, icRes_FF, muRes_FF, faNumGlo, faNumGlo); % M^{1}
